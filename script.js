c57a7a00f5069c7b28ae0cbad430ab3ffe6546b8
Ebano Assumpção <ebanoassumpcao@gmail.com>
2021-01-24 04:38:32 -0200

    site da mirra v1

---
+++ script.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+  $('.carousel').slick({
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	autoplay: true,
+	autoplaySpeed: 2000,
+	arrows: true,
+	dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear',
+  pauseOnFocus: false,
+  });
+}); 
